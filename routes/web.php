478592ce1e8bdb02b380e20eef4a1ac14b2125be
lintangsekar <134347531+lintangsekar@users.noreply.github.com>
2023-11-27 13:42:09 +0700

    booking

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/booking', function () {
+    return view('booking');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
